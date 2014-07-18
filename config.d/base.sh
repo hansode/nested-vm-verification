e6364516ce0861a376edf6195a32e7108df48968
Masahito Yoshida <masahito@axsh.net>
2014-07-18 13:51:36 +0900

    fix deploy path.

--- config.d/base.sh
+++ config.d/base.sh
@@ -12,7 +12,7 @@ set -x
 su - vagrant -c "$SHELL -ex" <<EOS
   curl -fSkL https://raw.github.com/hansode/env-bootstrap/master/build-personal-env.sh | bash
 
-  [[ -d work/repos/git/github.com ]] || {
-    git clone https://github.com/hansode/vagrant-setup work/repos/git/github.com
+  [[ -d work/repos/git/github.com/vagrant-setup ]] || {
+    git clone https://github.com/hansode/vagrant-setup work/repos/git/github.com/vagrant-setup
   }
 EOS
