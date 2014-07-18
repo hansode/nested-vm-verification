04cca1d9b3197b56c32588aec590c75c1597e3f5
Masahito Yoshida <masahito@axsh.net>
2014-07-18 13:25:11 +0900

    first commit

---
+++ config.d/base.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+set -e
+set -o pipefail
+set -x
+
+# Do some changes ...
+
+su - vagrant -c "$SHELL -ex" <<EOS
+  curl -fSkL https://raw.github.com/hansode/env-bootstrap/master/build-personal-env.sh | bash
+
+  [[ -d work/repos/git/github.com ]] || {
+    git clone https://github.com/hansode/vagrant-setup work/repos/git/github.com
+  }
+EOS
